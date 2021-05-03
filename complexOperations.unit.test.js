4c316b5efa990242d57105a3f20d90bfbb5f8365
Martina Sierra <martinasierra94@gmail.com>
2021-05-03 13:34:25 -0300

    Add more tests

--- complexOperations.unit.test.js
+++ complexOperations.unit.test.js
@@ -36,7 +36,7 @@ describe('complexOperation - Unit Tests', () => {
     describe('calculateArea', () => {
         beforeEach(() => {
             jest.restoreAllMocks();
-          });
+        });
         
         it('calculateArea', () => {
             jest.spyOn(basicOperations, 'isSupportedFigure').mockReturnValue(true);
@@ -48,22 +48,73 @@ describe('complexOperation - Unit Tests', () => {
         it('calculateArea', () => {
             jest.spyOn(basicOperations, 'isSupportedFigure').mockReturnValue(true);
             jest.spyOn(basicOperations, 'isNumber').mockReturnValue(true);
-            jest.spyOn(basicOperations, 'multip').mockReturnValue(32);
-            expect(complexOperations.calculateArea('square', 4, 8)).toBe(32);
+            jest.spyOn(basicOperations, 'multip').mockReturnValue(16);
+            expect(complexOperations.calculateArea('square', 4, 4)).toBe(16);
         });
 
         it('calculateArea', () => {
             jest.spyOn(basicOperations, 'isSupportedFigure').mockReturnValue(true);
             jest.spyOn(basicOperations, 'isNumber').mockReturnValue(true);
-            jest.spyOn(basicOperations, 'multip').mockReturnValue(32);
-            expect(complexOperations.calculateArea('triangle', 4, 8)).toBe(32);
+            jest.spyOn(basicOperations, 'multip').mockReturnValue(12);
+            jest.spyOn(basicOperations, 'division').mockReturnValue(6);
+            expect(complexOperations.calculateArea('triangle', 4, 3)).toBe(6);
         });
 
         it('calculateArea', () => {
             jest.spyOn(basicOperations, 'isSupportedFigure').mockReturnValue(true);
             jest.spyOn(basicOperations, 'isNumber').mockReturnValue(true);
-            jest.spyOn(basicOperations, 'multip').mockReturnValue(32);
-            expect(complexOperations.calculateArea('circle', 4)).toBe(32);
+            jest.spyOn(basicOperations, 'exponent').mockReturnValue(16);
+            expect(complexOperations.calculateArea('circle', 4)).toBe(50.26548245743669);
+        });
+
+        it('calculateArea', () => {
+            jest.spyOn(basicOperations, 'isSupportedFigure').mockReturnValue(false);
+            jest.spyOn(basicOperations, 'isNumber').mockReturnValue(true);
+            expect(complexOperations.calculateArea('hexagon', 4, 8)).toEqual('hexagon is not supported');
+        });
+
+        it('calculateArea', () => {
+            jest.spyOn(basicOperations, 'isSupportedFigure').mockReturnValue(true);
+            jest.spyOn(basicOperations, 'isNumber').mockReturnValue(false);
+            expect(complexOperations.calculateArea('rectangle', 'four', 'eight')).toEqual('number1 and number2 should be numbers');
+        });
+    });
+
+    describe('sumGreaterThan', () => {
+        beforeEach(() => {
+            jest.restoreAllMocks();
+        });
+        it('first test for sumGreaterThan', () => {
+            jest.spyOn(basicOperations, 'isNumber').mockReturnValue(true);
+            jest.spyOn(basicOperations, 'sum').mockReturnValue(5);
+            expect(complexOperations.sumGreaterThan(4, 1, 6)).toEqual('5 is less than 6');
+        });
+      });
+
+    describe('intersectionBetweenArrays', () => {
+        beforeEach(() => {
+            jest.restoreAllMocks();
+        });
+        it('test for intersectionBetweenArrays', () => {
+    
+        });
+    });
+
+    describe('sortArrayOfObjectsByKey', () => {
+        beforeEach(() => {
+            jest.restoreAllMocks();
+        });
+        it('test for sortArrayOfObjectsByKey', () => {
+    
+        });
+    });
+
+    describe('numberOfOddAndEvenNumbers', () => {
+        beforeEach(() => {
+            jest.restoreAllMocks();
+        });
+        it('test for numberOfOddAndEvenNumbers', () => {
+    
         });
     });
 });
